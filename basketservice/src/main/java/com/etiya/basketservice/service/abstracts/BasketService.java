829df83c1c72ccb9dfa3aa65b2610b925099b6e4
BerkaySimsek1 <berkay.simsek.098@gmail.com>
2025-11-11 19:25:17 +0300

    add campaigns to basket change basket feature

--- basketservice/src/main/java/com/etiya/basketservice/service/abstracts/BasketService.java
+++ basketservice/src/main/java/com/etiya/basketservice/service/abstracts/BasketService.java
@@ -6,12 +6,13 @@ import java.util.Map;
 
 public interface BasketService {
     void add(int billingAccountId, String productOfferId);
-
+    void addByCampaignProduct(int billingAccountId, int campaignProductId);
     // YENİ: Sepetten tek bir kalemi siler (BasketItem'ın kendi ID'si ile)
     void deleteItem(int billingAccountId, String basketItemId);
 
     // YENİ: Belirli bir billingAccount'a ait sepeti temizler
     void clearBasket(int billingAccountId);
-
+    Basket getByBillingAccountId(int billingAccountId);
+    void addByCampaign(int billingAccountId, int campaignId);
     Map<String, Basket> getAll();
 }
