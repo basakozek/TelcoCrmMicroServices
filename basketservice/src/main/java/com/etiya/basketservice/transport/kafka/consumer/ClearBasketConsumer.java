af3f7f10cc0b14e454ff00b3443d44ee1be1fa39
aus20 <115422437+aus20@users.noreply.github.com>
2025-11-17 12:04:11 +0300

    bean eklendi

--- basketservice/src/main/java/com/etiya/basketservice/transport/kafka/consumer/ClearBasketConsumer.java
+++ basketservice/src/main/java/com/etiya/basketservice/transport/kafka/consumer/ClearBasketConsumer.java
@@ -5,9 +5,11 @@ import com.etiya.common.events.ClearBasketEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.function.Consumer;
 
+@Configuration
 public class ClearBasketConsumer {
     private final BasketService basketService;
     private static final Logger LOGGER = LoggerFactory.getLogger(ClearBasketConsumer.class);
