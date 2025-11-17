6a933e9da76a48a81c8924022610c348d96bbeb7
Başak Özek <basakozek3@gmail.com>
2025-11-17 15:57:54 +0300

    .

--- salesservice/src/main/java/com/etiya/salesservice/service/dtos/OrderProductDetailResponse.java
+++ salesservice/src/main/java/com/etiya/salesservice/service/dtos/OrderProductDetailResponse.java
@@ -1,9 +1,16 @@
 package com.etiya.salesservice.service.dtos;
 
 import com.etiya.salesservice.domain.ProductConfiguration;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderProductDetailResponse {
     private String id;
     private String productOfferId;
@@ -13,74 +20,4 @@ public class OrderProductDetailResponse {
     private int addressId;
     private List<ProductConfiguration> configuration;
 
-    public OrderProductDetailResponse() {}
-
-    public OrderProductDetailResponse(String id, String productOfferId, String productOfferName,
-                                      String status, int billingAccountId, int addressId,
-                                      List<ProductConfiguration> configuration) {
-        this.id = id;
-        this.productOfferId = productOfferId;
-        this.productOfferName = productOfferName;
-        this.status = status;
-        this.billingAccountId = billingAccountId;
-        this.addressId = addressId;
-        this.configuration = configuration;
-    }
-
-    // Getters and Setters
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getProductOfferId() {
-        return productOfferId;
-    }
-
-    public void setProductOfferId(String productOfferId) {
-        this.productOfferId = productOfferId;
-    }
-
-    public String getProductOfferName() {
-        return productOfferName;
-    }
-
-    public void setProductOfferName(String productOfferName) {
-        this.productOfferName = productOfferName;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public int getBillingAccountId() {
-        return billingAccountId;
-    }
-
-    public void setBillingAccountId(int billingAccountId) {
-        this.billingAccountId = billingAccountId;
-    }
-
-    public int getAddressId() {
-        return addressId;
-    }
-
-    public void setAddressId(int addressId) {
-        this.addressId = addressId;
-    }
-
-    public List<ProductConfiguration> getConfiguration() {
-        return configuration;
-    }
-
-    public void setConfiguration(List<ProductConfiguration> configuration) {
-        this.configuration = configuration;
-    }
 }
