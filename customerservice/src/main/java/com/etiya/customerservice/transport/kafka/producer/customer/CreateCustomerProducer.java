a91437e12cf5ddad223a68783e4fd2be70f39a4c
BerkaySimsek1 <berkay.simsek.098@gmail.com>
2025-11-20 09:14:20 +0300

    cleaning code

--- customerservice/src/main/java/com/etiya/customerservice/transport/kafka/producer/customer/CreateCustomerProducer.java
+++ customerservice/src/main/java/com/etiya/customerservice/transport/kafka/producer/customer/CreateCustomerProducer.java
@@ -8,23 +8,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CreateCustomerProducer {
-    //private final KafkaTemplate<String, CreateCustomerEvent> kafkaTemplate;
     private final StreamBridge streamBridge;
     private static final Logger LOGGER = LoggerFactory.getLogger(CreateCustomerProducer.class);
 
     public CreateCustomerProducer(/*KafkaTemplate<String, CreateCustomerEvent> kafkaTemplate, */ StreamBridge streamBridge) {
         this.streamBridge = streamBridge;
-        //this.kafkaTemplate = kafkaTemplate;
     }
 
     public void produceCustomerCreated(CreateCustomerEvent event) {
         streamBridge.send("customerCreated-out-0", event);
         LOGGER.info(String.format("Customer created event => %s", event.customerId()));
-
-//       Message<CreateCustomerEvent> message = MessageBuilder.withPayload(event)
-//               .setHeader(KafkaHeaders.TOPIC, "create-customer").build();
-//
-//       kafkaTemplate.send(message);
     }
 }
 
